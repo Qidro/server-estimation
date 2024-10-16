c8c1d2e14491c441540852ca195c9c52fc98b17f
Osincev Pavel <pavel.osincev04@mail.ru>
2024-10-16 15:57:06 +0500

    Create Controller for get a list users

---
+++ server-estimation/Controllers/SetUsersController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using server_estimation.Contracts;
+using server_estimation.DataAccess;
+using server_estimation.Models;
+using System.Xml.Linq;
+
+namespace server_estimation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SetUsersController : Controller
+    {
+        private readonly EstimationDbContext _dbcontext;
+        public SetUsersController(EstimationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> SetUserList()
+        {
+
+            try
+            {
+                // получаем значени из БД
+                var allRows = _dbcontext.Users.ToList();
+
+                // создаем коллецию для хранения 
+                 var UsersList = new List<СhangeUsers>();
+
+                foreach (var theUsers in allRows)
+                {
+                    //Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.Email);
+                    UsersList.Add(new СhangeUsers
+                    {
+                        Login = theUsers.Login,
+                        FullName  = theUsers.LastName +" " + theUsers.FirstName + " " + theUsers.Patronymic,
+                        Email = theUsers.Email,
+                        ConfirmedEmail = theUsers.ConfirmedEmail
+                    });
+
+                }
+
+                Console.WriteLine("Данные о пользователях были получены");
+                return Ok(UsersList);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: ", ex.ToString());
+                return StatusCode(400);
+            }
+        }
+
+
+        public class СhangeUsers
+        {
+            public string Login { get; set; }
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public bool ConfirmedEmail { get; set; }
+        }
+    }
+}
