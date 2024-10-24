3c4561796ad520cc9efca1521d12310950558b1c
Osincev Pavel <pavel.osincev04@mail.ru>
2024-10-24 11:04:37 +0500

    Create controller for edit data user

---
+++ server-estimation/Contracts/EditUsers.cs
@@ -0,0 +1,4 @@
+﻿namespace server_estimation.Contracts
+{
+    public record EditUsers(int Id, string Login, string FirstName, string LastName, string Patronymic, string Email, string Role, string Divisions, string JobTitle,  string Password);
+}
