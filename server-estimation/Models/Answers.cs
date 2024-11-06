696047f6d944038cf6185a25a412399b3d6984d7
Osincev Pavel <pavel.osincev04@mail.ru>
2024-11-06 12:17:53 +0500

    Edit contract for Survey and create models dor a new tables

---
+++ server-estimation/Models/Answers.cs
@@ -0,0 +1,15 @@
+﻿namespace server_estimation.Models
+{
+    public class Answers
+    {
+        public int Id { get; set; }
+
+        public string question { get; set; }
+
+        public string comment { get; set; }
+
+        public int points { get; set; }
+
+        Question questionId { get; set; }
+    }
+}
---
+++ server-estimation/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿namespace server_estimation.Models
+{
+    public class Question
+    {
+        public Question(string TitleQuestion, string Description) {
+            titleQuestion = TitleQuestion;
+            description = Description;
+        }
+        public int Id { get; set; }
+
+        public string titleQuestion { get; set; }
+
+        public string description { get; set; }
+
+        public Survey survey { get; set; }
+    }
+}
